e74c5bda64f142f5927d68027032e149f8880b07
xamlo721 <thexamlomaximal@gmail.com>
2023-09-20 20:30:05 +0300

    add comment for SecurityEvent class

--- src/items/SecurityEvent.cpp
+++ src/items/SecurityEvent.cpp
@@ -1,17 +1,62 @@
 #include "SecurityEvent.h"
 
+/**
+ * @brief SecurityEvent - базовый конструктор по умолчанию
+ * Он необходим для того, чтобы объект можно было хранить
+ * в контейнерах Qlist, QMap и т.д
+ */
+SecurityEvent::SecurityEvent() {
+    this->id = 0;
+}
+
+/**
+ * @brief SecurityEvent - конструктор копирования объектов
+ * Достаточно часто неявно вызывается, особенно при передаче
+ * в функцию в качестве параметра и т.д
+ * @param other - объект, используемый как источник данных
+ */
 SecurityEvent::SecurityEvent(quint32 id, QString text, QObject *parent) : QObject(parent) {
     this->id = id;
     this->text = text;
 }
 
-
-SecurityEvent::SecurityEvent(const SecurityEvent& other)
-    : QObject(other.parent()), id(other.id), text(other.text) {
-
-
+/**
+ * @brief SecurityEvent
+ * @param id - Цифровой целочисленный ID для однозначной идентификации и учёта объекта
+ * @param text - Текстовое описание события, задаваемое администратором
+ * @param parent - родительский объект в иерархии Qt.
+ * Выстраиваение древовидной структуры объектов помогает потом не терять память
+ * из-за встроенного в Qt garbage collector.
+ * (когда вызывается деструктор родительского объекта,
+ * вызовется деструктор дочерних объектов)
+ */
+SecurityEvent::SecurityEvent(const SecurityEvent& copy)
+    : QObject(copy.parent()), id(copy.id), text(copy.text) {
+    //NO-OP
 }
 
+/**
+ * @brief getId - Получить ID события
+ * @return
+ */
+quint32 SecurityEvent::getId()  const{
+    return this->id;
+}
+
+/**
+ * @brief getText - Получить текст события
+ * @return
+ */
+QString SecurityEvent::getText()  const{
+    return this->text;
+}
+
+
+/**
+ * @brief operator =
+ * @param other
+ * @return
+ */
 SecurityEvent& SecurityEvent::operator=(const SecurityEvent& other) {
     if (&other == this)
         return *this;
@@ -23,28 +68,58 @@ SecurityEvent& SecurityEvent::operator=(const SecurityEvent& other) {
     return *this;
 }
 
+/**
+ * @brief operator ==
+ * @param other
+ * @return
+ */
 bool SecurityEvent::operator==(const SecurityEvent& other) const {
     return (id == other.id
          && text == other.text);
 }
 
+/**
+ * @brief operator !=
+ * @param other
+ * @return
+ */
 bool SecurityEvent::operator!=(const SecurityEvent& other) const {
     return !(*this == other);
 }
 
+/**
+ * @brief operator <
+ * @param other
+ * @return
+ */
 bool SecurityEvent::operator<(const SecurityEvent& other) const {
-    return this->id < other.id;
+    return !(*this > other);
 }
 
+/**
+ * @brief operator >
+ * @param other
+ * @return
+ */
 bool SecurityEvent::operator>(const SecurityEvent& other) const {
     return this->id > other.id;
 }
 
-
-quint32 SecurityEvent::getId() {
-    return this->id;
+/**
+ * @brief operator >=
+ * @param other
+ * @return
+ */
+bool SecurityEvent::operator>=(const SecurityEvent& other) const {
+    return !(*this < other);
 }
 
-QString SecurityEvent::getText() {
-    return this->text;
+/**
+ * @brief operator <=
+ * @param other
+ * @return
+ */
+bool SecurityEvent::operator<=(const SecurityEvent& other) const {
+    return !(*this > other);
 }
+
--- src/items/SecurityEvent.h
+++ src/items/SecurityEvent.h
@@ -2,29 +2,111 @@
 #define SECURITYEVENT_H
 
 #include <QObject>
-
+/**
+ * @brief The SecurityEvent class - Data-class, описывающий
+ * понятие события информационной безопасности
+ *
+ * Например, это отключение сети, НСД или попытка взлома.
+ */
 class SecurityEvent : public QObject {
 
     Q_OBJECT
 
-    public:
-        explicit SecurityEvent(quint32 id, QString text, QObject *parent = nullptr);
-        SecurityEvent(const SecurityEvent& other);
-        SecurityEvent() {}
-        SecurityEvent& operator=(const SecurityEvent& other);
-        bool operator==(const SecurityEvent& other) const;
-        bool operator!=(const SecurityEvent& other) const;
-        bool operator<(const SecurityEvent& other) const;
-        bool operator>(const SecurityEvent& other) const;
-
-        quint32 getId();
-        QString getText();
-
     private:
+        ///Цифровой целочисленный ID для однозначной идентификации и учёта объекта
         quint32 id;
+        ///Текстовое описание события, задаваемое администратором
         QString text;
 
-    signals:
+    public:
+        /**
+         * @brief SecurityEvent - базовый конструктор по умолчанию
+         * Он необходим для того, чтобы объект можно было хранить
+         * в контейнерах Qlist, QMap и т.д
+         */
+        SecurityEvent();
+
+        /**
+         * @brief SecurityEvent - конструктор копирования объектов
+         * Достаточно часто неявно вызывается, особенно при передаче
+         * в функцию в качестве параметра и т.д
+         * @param other - объект, используемый как источник данных
+         */
+        SecurityEvent(const SecurityEvent& copy);
+
+        /**
+         * @brief SecurityEvent
+         * @param id - Цифровой целочисленный ID для однозначной идентификации и учёта объекта
+         * @param text - Текстовое описание события, задаваемое администратором
+         * @param parent - родительский объект в иерархии Qt.
+         * Выстраиваение древовидной структуры объектов помогает потом не терять память
+         * из-за встроенного в Qt garbage collector.
+         * (когда вызывается деструктор родительского объекта,
+         * вызовется деструктор дочерних объектов)
+         */
+        SecurityEvent(quint32 id, QString text, QObject *parent = nullptr);
+
+        /**
+         * @brief getId - Получить ID события
+         * @return
+         */
+        quint32 getId() const;
+
+        /**
+         * @brief getText - Получить текст события
+         * @return
+         */
+        QString getText() const;
+
+        /**
+         * @brief operator =
+         * @param other
+         * @return
+         */
+        SecurityEvent& operator=(const SecurityEvent& other);
+
+        /**
+         * @brief operator ==
+         * @param other
+         * @return
+         */
+        bool operator==(const SecurityEvent& other) const;
+
+        /**
+         * @brief operator !=
+         * @param other
+         * @return
+         */
+        bool operator!=(const SecurityEvent& other) const;
+
+        /**
+         * @brief operator <
+         * @param other
+         * @return
+         */
+        bool operator<(const SecurityEvent& other) const;
+
+        /**
+         * @brief operator >
+         * @param other
+         * @return
+         */
+        bool operator>(const SecurityEvent& other) const;
+
+        /**
+         * @brief operator >=
+         * @param other
+         * @return
+         */
+        bool operator>=(const SecurityEvent& other) const;
+
+        /**
+         * @brief operator <=
+         * @param other
+         * @return
+         */
+        bool operator<=(const SecurityEvent& other) const;
+
 
 };
 
