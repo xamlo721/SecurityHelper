fbaad7cdef7907967ab7bc14b705249ba12b4377
xamlo721 <thexamlomaximal@gmail.com>
2023-10-20 17:18:04 +0300

    update AdminEventsWidget

--- src/ui/admin/AdminEventsWidget.cpp
+++ src/ui/admin/AdminEventsWidget.cpp
@@ -1,14 +1,44 @@
 #include "AdminEventsWidget.h"
 #include "ui_AdminEventsWidget.h"
 
-AdminEventsWidget::AdminEventsWidget(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::AdminEventsWidget)
-{
+AdminEventsWidget::AdminEventsWidget(QWidget *parent) : QWidget(parent), ui(new Ui::AdminEventsWidget) {
     ui->setupUi(this);
+    QObject::connect(this->ui->pushButton_addEvent, &QPushButton::clicked, this, &AdminEventsWidget::onAddEventButtonPressed);
+    QObject::connect(this->ui->pushButton_editEvent, &QPushButton::clicked, this, &AdminEventsWidget::onEditEventButtonPressed);
+    QObject::connect(this->ui->pushButton_deleteSelectedEvents, &QPushButton::clicked, this, &AdminEventsWidget::onDelEventButtonPressed);
+
 }
 
-AdminEventsWidget::~AdminEventsWidget()
-{
+AdminEventsWidget::~AdminEventsWidget() {
     delete ui;
+}
+
+/**
+ * @brief clearEvents - очищает визуальный список событий
+ */
+void AdminEventsWidget::clearEvents() {
+    this->ui->scrollArea_events->clear();
+}
+
+/**
+ * @brief addEvent - добавляет в отображаемый список событий новый виджет
+ * @param event - заранее сделанный виджет
+ */
+void AdminEventsWidget::addEvent(SelectedWidget * event) {
+    QObject::connect(event, &SelectedWidget::signalSelected, this, &AdminEventsWidget::signaEventClicked);
+    this->ui->scrollArea_events->addWidget(event->getId(), event);
+}
+
+void AdminEventsWidget::onAddEventButtonPressed() {
+    this->ui->scrollArea_events->unselect();
+    emit signalAddEventClicked();
+}
+
+void AdminEventsWidget::onEditEventButtonPressed() {
+    //TODO: открыть окно редактирования
+
+}
+
+void AdminEventsWidget::onDelEventButtonPressed() {
+    emit signalDelEventClicked(this->ui->scrollArea_events->getSelectedWidgetID());
 }
--- src/ui/admin/AdminEventsWidget.h
+++ src/ui/admin/AdminEventsWidget.h
@@ -2,21 +2,71 @@
 #define ADMINEVENTSWIDGET_H
 
 #include <QWidget>
+#include "src/ui/SelectedWidget.h"
 
 namespace Ui {
-class AdminEventsWidget;
+    class AdminEventsWidget;
 }
 
-class AdminEventsWidget : public QWidget
-{
+class AdminEventsWidget : public QWidget {
+
     Q_OBJECT
 
-public:
-    explicit AdminEventsWidget(QWidget *parent = nullptr);
-    ~AdminEventsWidget();
 
-private:
-    Ui::AdminEventsWidget *ui;
+    private:
+        Ui::AdminEventsWidget *ui;
+
+    public:
+        explicit AdminEventsWidget(QWidget *parent = nullptr);
+        ~AdminEventsWidget();
+
+        /**
+         * @brief clearEvents - очищает визуальный список событий
+         */
+        void clearEvents();
+
+
+        /**
+         * @brief addEvent - добавляет в отображаемый список событий новый виджет
+         * @param event - заранее сделанный виджет
+         */
+        void addEvent(SelectedWidget * event);
+
+    signals:
+
+        /**
+         * @brief signalAddEventClicked - сигнал вызывается
+         * при кажатии администратором на кнопку добавления события
+         */
+        void signalAddEventClicked();
+
+        /**
+         * @brief signalEditEventClicked - сигнал вызывается
+         * при кажатии администратором на кнопку редактирования события
+         * @param eventID - ID выбранного события
+         * @param containedEvents - Новый набор входящих событий
+         */
+        void signalEditEventClicked(quint32 eventID, QString categoryName);
+
+        /**
+         * @brief signalDelEventClicked - сигнал вызывается
+         * при кажатии администратором на кнопку удаления события
+         * @param eventID - ID выбранного события
+         */
+        void signalDelEventClicked(quint32 eventID);
+
+        /**
+         * @brief signaEventClicked - сигнал вызывается
+         * при кажатии администратором на событие
+         * @param eventID - ID события
+         */
+        void signalEventClicked(quint32 eventID);
+
+    private slots:
+        void onAddEventButtonPressed();
+        void onEditEventButtonPressed();
+        void onDelEventButtonPressed();
+
 };
 
 #endif // ADMINEVENTSWIDGET_H
