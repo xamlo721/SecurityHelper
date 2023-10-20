62158a677923c8a5ea43d434779c02c585909f40
xamlo721 <thexamlomaximal@gmail.com>
2023-10-20 23:30:51 +0300

    add Admin Incident Widget background components

--- src/ui/admin/AdminIncidentsWidget.cpp
+++ src/ui/admin/AdminIncidentsWidget.cpp
@@ -1,14 +1,63 @@
 #include "AdminIncidentsWidget.h"
 #include "ui_AdminIncidentsWidget.h"
 
-AdminIncidentsWidget::AdminIncidentsWidget(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::AdminIncidentsWidget)
-{
+AdminIncidentsWidget::AdminIncidentsWidget(QWidget *parent) : QWidget(parent), ui(new Ui::AdminIncidentsWidget) {
     ui->setupUi(this);
+    QObject::connect(this->ui->pushButton_addIncident, &QPushButton::clicked, this, &AdminIncidentsWidget::onAddIncidentButtonPressed);
+    QObject::connect(this->ui->pushButton_editIncident, &QPushButton::clicked, this, &AdminIncidentsWidget::onEditIncidentButtonPressed);
+    QObject::connect(this->ui->pushButton_deleteIncident, &QPushButton::clicked, this, &AdminIncidentsWidget::onDelIncidentButtonPressed);
+
+    QObject::connect(this->ui->pushButton_saveIncident, &QPushButton::clicked, this, &AdminIncidentsWidget::onSaveIncidentButtonPressed);
+
 }
 
-AdminIncidentsWidget::~AdminIncidentsWidget()
-{
+void AdminIncidentsWidget::clearIncidents() {
+    this->ui->scrollArea_incidents->clear();
+}
+
+void AdminIncidentsWidget::addIncident(SelectedWidget *incident) {
+    QObject::connect(incident, &SelectedWidget::signalSelected, this, &AdminIncidentsWidget::signalIncidentClicked);
+    this->ui->scrollArea_incidents->addWidget(incident->getId(), incident);
+}
+
+void AdminIncidentsWidget::clearAvailableEvents() {
+    this->ui->scrollArea_incidents_freeEvents->clear();
+}
+
+void AdminIncidentsWidget::addAvalilableEvent(SelectedWidget * event) {
+    QObject::connect(event, &SelectedWidget::signalSelected, this, &AdminIncidentsWidget::signaAvailableEventClicked);
+    this->ui->scrollArea_incidents_freeEvents->addWidget(event->getId(), event);
+}
+
+void AdminIncidentsWidget::clearSelectedEvents() {
+    this->ui->scrollArea_incidents_includedEvent->clear();
+
+}
+
+void AdminIncidentsWidget::addSelectedEvent(SelectedWidget * event) {
+    QObject::connect(event, &SelectedWidget::signalSelected, this, &AdminIncidentsWidget::signalSelectedEventClicked);
+    this->ui->scrollArea_incidents_includedEvent->addWidget(event->getId(), event);
+
+}
+
+void AdminIncidentsWidget::onAddIncidentButtonPressed() {
+    this->ui->scrollArea_incidents->unselect();
+    emit signalAddCategoryClicked();
+}
+
+void AdminIncidentsWidget::onEditIncidentButtonPressed() {
+    //TODO: открыть окно редактирования
+}
+
+void AdminIncidentsWidget::onDelIncidentButtonPressed() {
+    emit signalDelIncidentClicked(this->ui->scrollArea_incidents->getSelectedWidgetID());
+}
+
+void AdminIncidentsWidget::onSaveIncidentButtonPressed() {
+    SelectedWidget * selected =  this->ui->scrollArea_incidents->getSelectedWidget();
+    emit signalEditIncidentClicked(selected->getId(), selected->getText());
+}
+
+AdminIncidentsWidget::~AdminIncidentsWidget() {
     delete ui;
 }
--- src/ui/admin/AdminIncidentsWidget.h
+++ src/ui/admin/AdminIncidentsWidget.h
@@ -2,21 +2,111 @@
 #define ADMININCIDENTSWIDGET_H
 
 #include <QWidget>
+#include "src/ui/SelectedWidget.h"
 
 namespace Ui {
-class AdminIncidentsWidget;
+    class AdminIncidentsWidget;
 }
 
-class AdminIncidentsWidget : public QWidget
-{
+class AdminIncidentsWidget : public QWidget {
+
     Q_OBJECT
 
-public:
-    explicit AdminIncidentsWidget(QWidget *parent = nullptr);
-    ~AdminIncidentsWidget();
+    private:
+        Ui::AdminIncidentsWidget *ui;
 
-private:
-    Ui::AdminIncidentsWidget *ui;
+    public:
+        AdminIncidentsWidget(QWidget *parent = nullptr);
+        ~AdminIncidentsWidget();
+
+        /**
+         * @brief clearIncidents - очищает на экране список инцидентов
+         */
+        void clearIncidents();
+
+        /**
+         * @brief clearAvailableEvents - очищает на экране список доступных событий
+         */
+        void clearAvailableEvents();
+
+        /**
+         * @brief clearSelectedEvents - очищает на экране список выбранных событий
+         */
+        void clearSelectedEvents();
+
+        /**
+         * @brief addIncident - добавляет в отображаемый
+         * список инцидентов новый виджет инцедента
+         * @param incident - заранее сделанный виджет
+         */
+        void addIncident(SelectedWidget * incident);
+
+        /**
+         * @brief addEvent - добавляет в отображаемый
+         * список доступных для выделения событий новый виджет
+         * @param event - заранее сделанный виджет
+         */
+        void addAvalilableEvent(SelectedWidget * event);
+
+        /**
+         * @brief addSelectedEvent - добавляет в отображаемый
+         * список выбранных событий новый виджет
+         * @param event - заранее сделанный виджет
+         */
+        void addSelectedEvent(SelectedWidget * event);
+
+    signals:
+
+        /**
+         * @brief signalIncidentClicked - сигнал вызывается
+         * при кажатии администратором на кнопку инцидента
+         * @param incidentId - ID выбранного инцидента
+         */
+        void signalIncidentClicked(quint32 incidentId);
+
+        /**
+         * @brief signalAddCategoryClicked - сигнал вызывается
+         * при кажатии администратором на кнопку добавления категории
+         */
+        void signalAddCategoryClicked();
+
+        /**
+         * @brief signalEditCategoryClicked - сигнал вызывается
+         * при кажатии администратором на кнопку редактирования категории
+         * @param categoryId - ID выбранной категории
+         * @param containedEvents - Новый набор входящих событий
+         */
+        void signalEditIncidentClicked(quint32 incidentId, QString categoryName);
+
+        /**
+         * @brief signalDelIncidentClicked - сигнал вызывается
+         * при кажатии администратором на кнопку удаления инцидента
+         * @param incidentId - ID выбранного инцидента
+         */
+        void signalDelIncidentClicked(quint32 incidentId);
+
+        /**
+         * @brief signalUnselectedEventClicked - сигнал вызывается
+         * при кажатии администратором на кнопку доступного события
+         * @param eventID - ID события
+         */
+        void signaAvailableEventClicked(quint32 eventID);
+
+        /**
+         * @brief signalSelectedEventClicked - сигнал вызывается
+         * при кажатии администратором на кнопку уже выбранного события
+         * @param eventID - ID события
+         */
+        void signalSelectedEventClicked(quint32 eventID);
+
+
+    private slots:
+        void onAddIncidentButtonPressed();
+        void onEditIncidentButtonPressed();
+        void onDelIncidentButtonPressed();
+        void onSaveIncidentButtonPressed();
+
+
 };
 
 #endif // ADMININCIDENTSWIDGET_H
