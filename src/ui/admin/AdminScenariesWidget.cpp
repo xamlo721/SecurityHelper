1d5611e97a324234dac5dc7c2f5b2cf78cdbeb63
xamlo721 <thexamlomaximal@gmail.com>
2023-10-21 10:42:08 +0300

    update Admin Scenaries Widget

--- src/ui/admin/AdminScenariesWidget.cpp
+++ src/ui/admin/AdminScenariesWidget.cpp
@@ -1,14 +1,63 @@
 #include "AdminScenariesWidget.h"
 #include "ui_AdminScenariesWidget.h"
 
-AdminScenariesWidget::AdminScenariesWidget(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::AdminScenariesWidget)
-{
+AdminScenariesWidget::AdminScenariesWidget(QWidget *parent) : QWidget(parent), ui(new Ui::AdminScenariesWidget) {
     ui->setupUi(this);
+    QObject::connect(this->ui->pushButton_addScenario, &QPushButton::clicked, this, &AdminScenariesWidget::onAddScenaryButtonPressed);
+    QObject::connect(this->ui->pushButton_editScenario, &QPushButton::clicked, this, &AdminScenariesWidget::onEditScenaryButtonPressed);
+    QObject::connect(this->ui->pushButton_deleteScenario, &QPushButton::clicked, this, &AdminScenariesWidget::onDelScenaryButtonPressed);
+
+    QObject::connect(this->ui->pushButton_saveScenario, &QPushButton::clicked, this, &AdminScenariesWidget::onSaveScenaryButtonPressed);
 }
 
-AdminScenariesWidget::~AdminScenariesWidget()
-{
+
+void AdminScenariesWidget::clearScenaries() {
+    this->ui->scrollArea_scenaries->clear();
+}
+
+void AdminScenariesWidget::addScenary(SelectedWidget *scenary) {
+    QObject::connect(scenary, &SelectedWidget::signalSelected, this, &AdminScenariesWidget::signalScenaryClicked);
+    this->ui->scrollArea_scenaries->addWidget(scenary->getId(), scenary);
+}
+
+void AdminScenariesWidget::clearAvailableIncidents() {
+    this->ui->scrollArea_scenaries_freeIncidents->clear();
+}
+
+void AdminScenariesWidget::addAvalilableIncident(SelectedWidget * incident) {
+    QObject::connect(incident, &SelectedWidget::signalSelected, this, &AdminScenariesWidget::signaAvailableIncidentClicked);
+    this->ui->scrollArea_scenaries_freeIncidents->addWidget(incident->getId(), incident);
+}
+
+void AdminScenariesWidget::clearSelectedIncidents() {
+    this->ui->scrollArea_scenaries_includedIncidents->clear();
+
+}
+
+void AdminScenariesWidget::addSelectedIncident(SelectedWidget * incident) {
+    QObject::connect(incident, &SelectedWidget::signalSelected, this, &AdminScenariesWidget::signalSelectedIncidentClicked);
+    this->ui->scrollArea_scenaries_includedIncidents->addWidget(incident->getId(), incident);
+
+}
+
+void AdminScenariesWidget::onAddScenaryButtonPressed() {
+    this->ui->scrollArea_scenaries->unselect();
+    emit signalAddScenaryClicked();
+}
+
+void AdminScenariesWidget::onEditScenaryButtonPressed() {
+    //TODO: открыть окно редактирования
+}
+
+void AdminScenariesWidget::onDelScenaryButtonPressed() {
+    emit signalDelScenaryClicked(this->ui->scrollArea_scenaries->getSelectedWidgetID());
+}
+
+void AdminScenariesWidget::onSaveScenaryButtonPressed() {
+    SelectedWidget * selected =  this->ui->scrollArea_scenaries->getSelectedWidget();
+    emit signalEditScenaryClicked(selected->getId(), selected->getText());
+}
+
+AdminScenariesWidget::~AdminScenariesWidget() {
     delete ui;
 }
--- src/ui/admin/AdminScenariesWidget.h
+++ src/ui/admin/AdminScenariesWidget.h
@@ -2,21 +2,100 @@
 #define ADMINSCENARIESWIDGET_H
 
 #include <QWidget>
+#include "src/ui/SelectedWidget.h"
 
 namespace Ui {
 class AdminScenariesWidget;
 }
 
-class AdminScenariesWidget : public QWidget
-{
+class AdminScenariesWidget : public QWidget {
+
     Q_OBJECT
 
-public:
-    explicit AdminScenariesWidget(QWidget *parent = nullptr);
-    ~AdminScenariesWidget();
+    private:
+        Ui::AdminScenariesWidget *ui;
 
-private:
-    Ui::AdminScenariesWidget *ui;
+    public:
+        AdminScenariesWidget(QWidget *parent = nullptr);
+        ~AdminScenariesWidget();
+
+        /**
+         * @brief clearScenaries - очищает визуальный список сценариев
+         */
+        void clearScenaries();
+
+        /**
+         * @brief clearAvailableIncidents - очищает визуальный список инцидентов
+         */
+        void clearAvailableIncidents();
+
+        /**
+         * @brief clearSelectedIncidents - очищает визуальный список выбранных инцидентов
+         */
+        void clearSelectedIncidents();
+
+        /**
+         * @brief addScenary - добавляет в отображаемый список сценарариев новый виджет
+         * @param scenary - заранее сделанный виджет
+         */
+        void addScenary(SelectedWidget * scenary);
+
+        /**
+         * @brief addAvalilableIncident - добавляет в отображаемый список доступных для выделения инцидентов новый виджет
+         * @param incident - заранее сделанный виджет
+         */
+        void addAvalilableIncident(SelectedWidget * incident);
+
+        /**
+         * @brief addSelectedIncident - добавляет в отображаемый список выбранных инцидентов новый виджет
+         * @param incident - заранее сделанный виджет
+         */
+        void addSelectedIncident(SelectedWidget * incident);
+
+    signals:
+
+        /**
+         * @brief signalScenaryClicked - сигнал вызывается при кажатии администратором на кнопку сценарариев
+         * @param scenaryId - ID выбранной категории
+         */
+        void signalScenaryClicked(quint32 scenaryId);
+
+        /**
+         * @brief signalAddScenaryClicked - сигнал вызывается при кажатии администратором на кнопку добавления сценарариев
+         */
+        void signalAddScenaryClicked();
+
+        /**
+         * @brief signalEditScenaryClicked - сигнал вызывается при кажатии администратором на кнопку редактирования сценарариев
+         * @param scenaryId - ID выбранного сценарария
+         * @param containedEvents - Новый набор входящих инцидентов
+         */
+        void signalEditScenaryClicked(quint32 scenaryId, QString scenaryName);
+
+        /**
+         * @brief signalDelScenaryClicked - сигнал вызывается при кажатии администратором на кнопку удаления категории
+         * @param scenaryId - ID выбранного сценарария
+         */
+        void signalDelScenaryClicked(quint32 scenaryId);
+
+        /**
+         * @brief signaAvailableIncidentClicked - сигнал вызывается при кажатии администратором на кнопку доступного инцидента
+         * @param incidentID - ID выбранного инцидента
+         */
+        void signaAvailableIncidentClicked(quint32 incidentID);
+
+        /**
+         * @brief signalSelectedIncidentClicked - сигнал вызывается при кажатии администратором на кнопку уже выбранного инцидента
+         * @param incidentID - ID выбранного инцидента
+         */
+        void signalSelectedIncidentClicked(quint32 incidentID);
+
+
+    private slots:
+        void onAddScenaryButtonPressed();
+        void onEditScenaryButtonPressed();
+        void onDelScenaryButtonPressed();
+        void onSaveScenaryButtonPressed();
 };
 
 #endif // ADMINSCENARIESWIDGET_H
